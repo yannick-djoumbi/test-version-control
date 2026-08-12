8fde273c1a932a58665eef6125bf3535b932d09f
yannick-djoumbi <yannick.djoumbi@cms.hhs.gov>
2026-08-12 15:20:44 -0500

    Synchronizing job testing-version-control-job from AWS Glue at 2026/08/12 20:20:44

---
+++ testing-version-control/testing-version-control-job.py
@@ -0,0 +1,20 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+print("Hello from AWS Glue!")
+print("Hello from AWS Glue!, his is version 2")
+
+job.commit()
